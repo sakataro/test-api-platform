aff6146e88f7b6258fadf511410d903d56f71d6f
Taro SAKAMOTO <gorgo0013@gmail.com>
2026-03-16 23:43:11 +0900

    php.ini追加

--- devenv.nix
+++ devenv.nix
@@ -12,4 +12,10 @@
     pkgs.symfony-cli
     pkgs.php85Packages.composer
   ];
+
+  # - Nix の PHP は拡張を動的 .so として管理しており、scan directory の ini で読み込んでいる。
+  # - Symfony CLI はプロジェクトルートの php.ini を -c で渡す
+  # ので、プロジェクトルートにphp.iniを追加するとデフォルトの scan directory が無効になり拡張が読み込まれなくなる。
+  # ↓を明示的に指定することで、Nix の拡張を確実に読み込ませつつルートのphp.iniも読むっぽい。
+  env.PHP_INI_SCAN_DIR = "${pkgs.php85}/lib";
 }
